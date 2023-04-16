33fff1547dda2bceb455ceed3258c5d06c5a4c4e
Julian Vick <JM@romtec.info>
2023-04-15 20:58:28 -0400

    Subscriber is working (subscriber.py is the old version)

--- subscriber.py
+++ subscriber.py
@@ -4,6 +4,7 @@ from tkinter import *
 import threading
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import time
 
 # Define MQTT broker parameters
 BROKER_HOST = "localhost"
@@ -16,12 +17,12 @@ TOPIC = "Room sensors"
 
 class Subscriber:
     def __init__(self, device_id, location):
-        self.humidity_bars = None
-        self.humidity_data = None
+        self.humidity_data = []
+        self.humidity_bars = plt.bar(range(len(self.humidity_data)), self.humidity_data)
         self.humidity_canvas = None
-        self.temp_line = None
+        self.temp_data = []
         self.temp_canvas = None
-        self.temp_data = None
+        self.temp_line = plt.plot(self.temp_data)
         self.device_id = device_id
         self.location = location
         self.client = mqtt.Client(client_id=self.device_id)
@@ -32,7 +33,7 @@ class Subscriber:
         self.temp_fig = None
         self.humidity_fig = None
 
-    def on_connect(self):
+    def on_connect(self, client, userdata, flags, rc):
         # Subscribe to the topic on connection
         self.client.subscribe(TOPIC)
 
@@ -45,30 +46,33 @@ class Subscriber:
             if self.temp_label:
                 self.temp_label.config(text=temp_str)
                 self.temp_data.append(data['temperature'])
-                self.temp_line.set_data(range(len(self.temp_data)), self.temp_data)
-                self.temp_fig.canvas.draw()
+                self.temp_line[0].set_xdata(range(len(self.temp_data)))
+                self.temp_line[0].set_ydata(self.temp_data)
+                self.temp_fig.canvas.draw_idle()
             if self.humidity_label:
                 self.humidity_label.config(text=humidity_str)
                 self.humidity_data.append(data['humidity'])
                 for i, bar in enumerate(self.humidity_bars):
                     bar.set_height(self.humidity_data[i])
-                self.humidity_fig.canvas.draw()
+                self.humidity_fig.canvas.draw_idle()
             print(f"{self.device_id} received: {message.payload.decode()}")
 
     def start(self):
         # Set up the GUI
         self.root = Tk()
-        self.root.geometry("700x400")
+        self.root.geometry("1000x700")
         self.root.title(f"Subscriber - {self.device_id}")
+        # If Temp data is not found
         self.temp_label = Label(self.root, text="Temperature: N/A")
         self.temp_label.pack()
 
         self.temp_fig = plt.Figure(figsize=(6, 4), dpi=100)
         self.temp_data = []
-        self.temp_line, = plt.plot(self.temp_data)
+        self.temp_line = plt.plot(self.temp_data)
         self.temp_canvas = FigureCanvasTkAgg(self.temp_fig, self.root)
         self.temp_canvas.get_tk_widget().pack(side=LEFT, fill=BOTH, expand=1)
 
+        # If humidity data is not found
         self.humidity_label = Label(self.root, text="Humidity: N/A")
         self.humidity_label.pack()
 
@@ -106,5 +110,7 @@ t3 = threading.Thread(target=sub3.start)
 t1.start()
 t2.start()
 t3.start()
+
+time.sleep(1)
 
 plt.show()
